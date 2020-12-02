d9af68f298410aef4c965c0206fb68c1ab2fff9f
audreyboyd <60332315+audreyboyd@users.noreply.github.com>
2020-12-02 12:15:28 -0500

    Player selection works, but does not show player name during game start.

---
+++ play.js
@@ -0,0 +1,35 @@
+function readyPlayer(){
+    var id = sessionStorage.getItem("id");
+    var input1 = document.getElementById("player1Name");
+    sessionStorage.setItem("player1", input1.value());
+    // if (id === "1"){
+    //     var input1 = document.getElementById("player1Name");
+    //     sessionStorage.setItem("player1", input1.value());
+    // }
+    // else if (id === "2"){
+    //     var input1 = document.getElementById("player1Name");
+    //     sessionStorage.setItem("player1", input1.value());
+    //     var input2 = document.getElementById("player2Name");
+    //     sessionStorage.setItem("player2", input2.value());
+    // }
+    // else if (id === "3"){
+    //     var input1 = document.getElementById("player1Name");
+    //     sessionStorage.setItem("player1", input1.value());
+    //     var input2 = document.getElementById("player2Name");
+    //     sessionStorage.setItem("player2", input2.value());
+    //     var input3 = document.getElementById("player3Name");
+    //     sessionStorage.setItem("player3", input3.value());
+    // }
+    // else if (id === "4"){
+    //     var input1 = document.getElementById("player1Name");
+    //     sessionStorage.setItem("player1", input1.value());
+    //     var input2 = document.getElementById("player2Name");
+    //     sessionStorage.setItem("player2", input2.value());
+    //     var input3 = document.getElementById("player3Name");
+    //     sessionStorage.setItem("player3", input3.value());
+    //     var input4 = document.getElementById("player4Name");
+    //     sessionStorage.setItem("player4", input4.value());
+    // }
+    var player1Text = sessionStorage.getItem("player1");
+    document.getElementById("playerText").innerHTML = player1Text + ", you're up!";
+}
---
+++ player.js
@@ -0,0 +1,36 @@
+function select1Player(){
+    var text = document.getElementById('playerText').style.display = "block";
+    var div1 = document.getElementById('player1Div').style.display = "block";
+    var div2 = document.getElementById('player2Div').style.display = "none";
+    var div3 = document.getElementById('player3Div').style.display = "none";
+    var div4 = document.getElementById('player4Div').style.display = "none";
+    var divStart = document.getElementById('divStart').style.display = "block";
+    sessionStorage.setItem('id', '1');
+}
+function select2Player(){
+    var text = document.getElementById('playerText').style.display = "block";
+    var div1 = document.getElementById('player1Div').style.display = "block";
+    var div2 = document.getElementById('player2Div').style.display = "block";
+    var div3 = document.getElementById('player3Div').style.display = "none";
+    var div4 = document.getElementById('player4Div').style.display = "none";
+    var divStart = document.getElementById('divStart').style.display = "block";
+    sessionStorage.setItem('id', '2');
+}
+function select3Player(){
+    var text = document.getElementById('playerText').style.display = "block";
+    var div1 = document.getElementById('player1Div').style.display = "block";
+    var div2 = document.getElementById('player2Div').style.display = "block";
+    var div3 = document.getElementById('player3Div').style.display = "block";
+    var div4 = document.getElementById('player4Div').style.display = "none";
+    var divStart = document.getElementById('divStart').style.display = "block";
+    sessionStorage.setItem('id', '3');
+}
+function select4Player(){
+    var text = document.getElementById('playerText').style.display = "block";
+    var div1 = document.getElementById('player1Div').style.display = "block";
+    var div2 = document.getElementById('player2Div').style.display = "block";
+    var div3 = document.getElementById('player3Div').style.display = "block";
+    var div4 = document.getElementById('player4Div').style.display = "block";
+    var divStart = document.getElementById('divStart').style.display = "block";
+    sessionStorage.setItem('id', '4');
+}
